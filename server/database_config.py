a8763287cab99f1ea3df6deaae78648068bb1518
contrust <2001raf@gmail.com>
2021-11-25 22:23:44 +0500

    added config for database server

---
+++ server/database_config.py
@@ -0,0 +1,9 @@
+from kvstorage.key_value_storage import KVStorage
+from server.config import Config
+
+
+class DatabaseConfig(Config):
+    def __init__(self, hostname: str = None, port: int = None,
+                 databases: dict[str, KVStorage] = None):
+        super().__init__(hostname, port)
+        self.databases = databases if databases else {}
--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kvstorage',
+    name='distributed-storage',
     version='0.1.0',
-    packages=find_packages(include=['kvserver',
-                                    'kvserver.*']),
+    packages=['kvstorage', 'server']
 )
