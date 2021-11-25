ddc7ec91ff611a36a6d40c9ae08bf697ad1da2ba
Artyom Borisov <55096070+contrust@users.noreply.github.com>
2021-11-25 12:30:16 +0500

    Delete client directory

--- client/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-def get_kvserver_response(server, port, request):
-    s = socket.create_connection((server, port))
-    s.sendall(request)
-    response = s.recv(4096)
-    str_response = response.decode('utf-8')
-    return str_response
