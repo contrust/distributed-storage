330257a151b9b32f3e6c5eba606d8ae3090909e6
contrust <2001raf@gmail.com>
2021-11-26 00:02:33 +0500

    simple client interface added

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='storage-storage-server',
     version='0.1.0',
-    packages=find_packages(include=['storage-server', 'storage-server.*'])
+    packages=find_packages(include=['storage-server', 'storage-server.*',
+                                    'storage-client', 'storage-client.*'])
 )
